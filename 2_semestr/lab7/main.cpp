873423c5e6b66dedee712d33d886d4cc2a9491cc
Джигир Александр <sasha-russkiy@inbox.ru>
2021-10-01 18:46:08 +0700

    комментарии к 7 лабе

--- 2_semestr/lab7/main.cpp
+++ 2_semestr/lab7/main.cpp
@@ -14,7 +14,8 @@ void dijkstra_route(int**, int, int, int, int*, stack <int>&);
 
 int main()
 {
-    // создаем файл для записи
+    setlocale(LC_ALL, "Russian");
+    // создаем файл для выходных данных
     ofstream out;
     out.open("out.txt");
 
@@ -23,27 +24,34 @@ int main()
 
     int** A = Read("data.txt", n, m);
 
+    // проверяем входной файл на валидность
     if(A == NULL | n != m)
     {
-        cout << "error";
-        out << "error";
+        cout << "Ошибка";
+        out << "Ошибка";
         return 0;
     }
+
     int s;
-    cout << "Start vertex: "; cin >> s;
+    cout << "Стартовая вершина: "; cin >> s;
     s--;
     int* d = dijkstra(A, n, s);
+    // выводим полученый маршрут расстояний в файл и консоль
     for(int i = 0; i < n; i++)
     {
         cout << s + 1 << " -> " << i + 1 << " = " << d[i] << endl;
         out << s + 1 << " -> " << i + 1 << " = " << d[i] << endl;
     }
+
     int end;
-    cout << "Final vertex: "; cin >> end;
+    cout << "Конечная вершина: "; cin >> end;
     end--;
+
     dijkstra_route(A, n, s, end, d, route);
-    cout << "Route: " << endl;
-    out << "Route: " << endl;
+
+    cout << "Маршрут: " << endl;
+    out << "Маршрут: " << endl;
+    // выводим машрут в консоль и файл
     while(!route.empty())
     {
         cout << " -> " << route.top();
@@ -68,6 +76,7 @@ int** Read(string file_name, int& n, int& m)
         int count = 0;
         int temp;
 
+        // считаем количество символов в файле
         while(!in.eof())
         {
             in >> temp;
@@ -82,6 +91,7 @@ int** Read(string file_name, int& n, int& m)
 
         while(!in.eof())
         {
+            // считаем количество пробелов в строке
             in.get(symbol);
             if(symbol == ' ') count_space++;
             if(symbol == '\n') break;
@@ -90,12 +100,13 @@ int** Read(string file_name, int& n, int& m)
         in.seekg(0, ios::beg);
         in.clear();
 
+        // вычисляем вершину графа
         n = count / (count_space + 1);
         m = count_space + 1;
 
-        int **x;
-        x = new int*[n];
+        int **x = new int*[n];
         
+        // записываем в двумерный массив верншины графа, где это номер начальной вершины, а m номер конечной вершины
         for(int i = 0; i < n; i++) x[i] = new int[m];
             for(int i = 0; i < n; i++)
             {
@@ -110,56 +121,69 @@ int** Read(string file_name, int& n, int& m)
     in.close();
     return NULL;
 }
+
+// реализация функции очистки памяти
 void Delete(int**& x, int n)
 {
     for (int i = 0; i < n; i++) delete[] x[i];
     delete[] x;
 }
+
+// реализация алгоритма дейкстры
 int* dijkstra(int** A, int n, int s)
 {
     const int inf = INT_MAX;
     int* d = new int[n];
     bool* v = new bool[n];
     int index, u;
+    
+    // заполняем массивы начальными данными
     for(int i = 0; i < n; i++)
     {
         d[i] = inf;
         v[i] = false;
     }
+
     d[s] = 0;
+    // идем от начальной вершины к первой вершине запоминаем расстояние идем ко второй вершине запоминаем расстояние и тд. пока не дойдем до конца, затем выбираем
+    // вершину с наименьшим расстоянием до исходной вершины и идем от нее к следующей так до конца, на выходе получаем кратчайший маршрут в виде длин
     for(int count = 0; count < n - 1; count++)
     {
         int min = inf;
         for(int i = 0; i < n; i++)
-        if(!v[i] && d[i] <= min)
-        {
-        min = d[i];
-        index = i;
-        }
+            if(!v[i] && d[i] <= min)
+            {
+                min = d[i];
+                index = i;
+            }
         u = index;
         v[u] = true;
         for(int i = 0; i < n; i++)
-        if(!v[i] && A[u][i] && d[u] != inf && d[u] + A[u][i] < d[i]) d[i] = d[u] + A[u][i];
+            if(!v[i] && A[u][i] && d[u] != inf && d[u] + A[u][i] < d[i]) 
+                d[i] = d[u] + A[u][i];
     }
     delete[] v;
     return d;
 }
+
+// реализация функции составления маршрута
 void dijkstra_route(int** A, int n, int s, int end, int* d, stack <int>& route)
 {
     route.push(end + 1);
     int weight = d[end];
+    // проходим по файлу и сравниваем значения в нем с значениями в массиве маршрута если совпадают запоминаем вершину, на выходе получаем путь
     while (end != s)
     {
         for (int i = 0; i < n; i++)
-        if (A[i][end] != 0)
-        {
-            int temp = weight - A[i][end];
-            if (temp == d[i])
+            if (A[i][end] != 0)
             {
-            weight = temp;
-            end = i;
-            route.push(i + 1);
+                int temp = weight - A[i][end];
+                if (temp == d[i])
+                {
+                    weight = temp;
+                    end = i;
+                    route.push(i + 1);
+                }
             }
-        }
     }
 }
